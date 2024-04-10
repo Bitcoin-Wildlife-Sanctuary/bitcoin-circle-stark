bce9782f7f6c81eb4cb35ecb6326e6b4b4caf470
weikengchen <w.k@berkeley.edu>
2024-04-10 14:47:52 -0700

    finish the channel implementation

--- src/channel.rs
+++ src/channel.rs
@@ -32,7 +32,7 @@ impl Channel {
 
         let mut hasher = Sha256::new();
         Digest::update(&mut hasher, &self.state);
-        Digest::update(&mut hasher, "0");
+        Digest::update(&mut hasher, &[0u8]);
         extract.copy_from_slice(hasher.finalize().as_slice());
 
         let mut hasher = Sha256::new();
@@ -47,7 +47,7 @@ impl Channel {
 
         let mut hasher = Sha256::new();
         Digest::update(&mut hasher, &self.state);
-        Digest::update(&mut hasher, "0");
+        Digest::update(&mut hasher, &[0u8]);
         extract.copy_from_slice(hasher.finalize().as_slice());
 
         let mut hasher = Sha256::new();
@@ -114,10 +114,12 @@ mod test {
     use crate::cfri::fields::{CM31, M31, QM31};
     use crate::channel::{Channel, ChannelGadget};
     use crate::channel_commit::Commitment;
+    use crate::channel_extract::ExtractorGadget;
     use bitcoin_script::script;
     use bitvm::treepp::*;
     use rand::{Rng, RngCore, SeedableRng};
     use rand_chacha::ChaCha20Rng;
+    use rust_bitcoin_u31_or_u30::{u31ext_equalverify, QM31 as QM31Gadget};
 
     #[test]
     fn test_mix_with_commitment() {
@@ -156,12 +158,12 @@ mod test {
         let mut prng = ChaCha20Rng::seed_from_u64(0);
 
         let channel_script = ChannelGadget::mix_with_qm31();
-        println!("Channel.mix_with_el() = {} bytes", channel_script.len());
+        println!("Channel.mix_with_qm31() = {} bytes", channel_script.len());
 
         let mut a = [0u8; 32];
         a.iter_mut().for_each(|v| *v = prng.gen());
 
-        let mut b = QM31(
+        let b = QM31(
             CM31(M31::reduce(prng.next_u64()), M31::reduce(prng.next_u64())),
             CM31(M31::reduce(prng.next_u64()), M31::reduce(prng.next_u64())),
         );
@@ -175,6 +177,71 @@ mod test {
             { a.to_vec() }
             { b }
             { channel_script.clone() }
+            { c.to_vec() }
+            OP_EQUAL
+        };
+        let exec_result = execute_script(script);
+        assert!(exec_result.success);
+    }
+
+    #[test]
+    fn test_draw_element_using_hint() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        let channel_script = ChannelGadget::draw_element_using_hint();
+        println!(
+            "Channel.draw_element_using_hint() = {} bytes",
+            channel_script.len()
+        );
+
+        let mut a = [0u8; 32];
+        a.iter_mut().for_each(|v| *v = prng.gen());
+
+        let mut channel = Channel::new(a);
+        let (b, hint) = channel.draw_element();
+
+        let c = channel.state;
+
+        let script = script! {
+            { ExtractorGadget::push_hint_qm31(&hint) }
+            { a.to_vec() }
+            { channel_script.clone() }
+            { b }
+            { u31ext_equalverify::<QM31Gadget>() }
+            { c.to_vec() }
+            OP_EQUAL
+        };
+        let exec_result = execute_script(script);
+        assert!(exec_result.success);
+    }
+
+    #[test]
+    fn test_draw_5queries_using_hint() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        let channel_script = ChannelGadget::draw_5queries_using_hint(15);
+        println!(
+            "Channel.draw_5queries_using_hint() = {} bytes",
+            channel_script.len()
+        );
+
+        let mut a = [0u8; 32];
+        a.iter_mut().for_each(|v| *v = prng.gen());
+
+        let mut channel = Channel::new(a);
+        let (b, hint) = channel.draw_5queries(15);
+
+        let c = channel.state;
+
+        let script = script! {
+            { ExtractorGadget::push_hint_5m31(&hint) }
+            { a.to_vec() }
+            { channel_script.clone() }
+            { b[4] } OP_EQUALVERIFY
+            { b[3] } OP_EQUALVERIFY
+            { b[2] } OP_EQUALVERIFY
+            { b[1] } OP_EQUALVERIFY
+            { b[0] } OP_EQUALVERIFY
             { c.to_vec() }
             OP_EQUAL
         };
