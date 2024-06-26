4e4ce18e212fe74d1e8f335448dd1d6a739136bd
weikengchen <w.k@berkeley.edu>
2024-06-27 06:13:36 +0800

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 
 #![deny(missing_docs)]
 
-extern crate core;
-
 use crate::treepp::pushable::{Builder, Pushable};
 use stwo_prover::core::circle::CirclePoint;
 use stwo_prover::core::fields::cm31::CM31;
