c424ac82e5d3cbc4150cc51778d87567beba71b3
weikengchen <w.k@berkeley.edu>
2024-08-23 12:00:13 +0800

    add tests to increase the coverage

--- src/air/bitcoin_script.rs
+++ src/air/bitcoin_script.rs
@@ -98,13 +98,16 @@ impl AirGadget {
 
 #[cfg(test)]
 mod test {
-    use crate::air::AirGadget;
+    use crate::air::{shifted_signed_mask_points, AirGadget};
     use crate::circle::CirclePointGadget;
     use crate::treepp::*;
+    use crate::utils::get_rand_qm31;
     use rand::{Rng, SeedableRng};
     use rand_chacha::ChaCha20Rng;
+    use rust_bitcoin_m31::qm31_equalverify;
     use stwo_prover::core::air::mask::shifted_mask_points;
     use stwo_prover::core::circle::{CirclePoint, SECURE_FIELD_CIRCLE_ORDER};
+    use stwo_prover::core::fields::qm31::QM31;
     use stwo_prover::core::poly::circle::CanonicCoset;
 
     #[test]
@@ -130,6 +133,68 @@ mod test {
                 { CirclePointGadget::equalverify() }
                 { expected[0][0] }
                 { CirclePointGadget::equalverify() }
+                OP_TRUE
+            };
+
+            let exec_result = execute_script(script);
+            assert!(exec_result.success);
+        }
+    }
+
+    #[test]
+    fn test_shifted_signed_mask_points() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        for _ in 0..100 {
+            let p = CirclePoint::get_point(prng.gen::<u128>() % SECURE_FIELD_CIRCLE_ORDER);
+
+            let mask = vec![vec![0, -1, 2]];
+            let domains = [CanonicCoset::new(5)];
+
+            let expected = shifted_signed_mask_points(&mask, &domains, p);
+            assert_eq!(expected.len(), 1);
+            assert_eq!(expected[0].len(), 3);
+
+            let script = script! {
+                { p }
+                { AirGadget::shifted_signed_mask_points(&mask, &domains) }
+                { expected[0][2] }
+                { CirclePointGadget::equalverify() }
+                { expected[0][1] }
+                { CirclePointGadget::equalverify() }
+                { expected[0][0] }
+                { CirclePointGadget::equalverify() }
+                OP_TRUE
+            };
+
+            let exec_result = execute_script(script);
+            assert!(exec_result.success);
+        }
+    }
+
+    #[test]
+    fn test_eval_from_partial_evals() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        for _ in 0..100 {
+            let a = get_rand_qm31(&mut prng);
+            let b = get_rand_qm31(&mut prng);
+            let c = get_rand_qm31(&mut prng);
+            let d = get_rand_qm31(&mut prng);
+
+            let mut res = a;
+            res += b * QM31::from_u32_unchecked(0, 1, 0, 0);
+            res += c * QM31::from_u32_unchecked(0, 0, 1, 0);
+            res += d * QM31::from_u32_unchecked(0, 0, 0, 1);
+
+            let script = script! {
+                { a }
+                { b }
+                { c }
+                { d }
+                { AirGadget::eval_from_partial_evals() }
+                { res }
+                qm31_equalverify
                 OP_TRUE
             };
 
--- src/air/mod.rs
+++ src/air/mod.rs
@@ -2,7 +2,10 @@ mod bitcoin_script;
 
 use crate::treepp::pushable::{Builder, Pushable};
 pub use bitcoin_script::*;
+use stwo_prover::core::circle::CirclePoint;
 use stwo_prover::core::fields::qm31::SecureField;
+use stwo_prover::core::poly::circle::CanonicCoset;
+use stwo_prover::core::ColumnVec;
 
 #[derive(Clone)]
 /// Hint for the two eval quotient results involved in the composition polynomial.
@@ -19,4 +22,22 @@ impl Pushable for CompositionHint {
         }
         builder
     }
+}
+
+/// A helper function to shift points, but with signed masks.
+pub fn shifted_signed_mask_points(
+    mask: &ColumnVec<Vec<isize>>,
+    domains: &[CanonicCoset],
+    point: CirclePoint<SecureField>,
+) -> ColumnVec<Vec<CirclePoint<SecureField>>> {
+    mask.iter()
+        .zip(domains.iter())
+        .map(|(mask_entry, domain)| {
+            let trace_step = CanonicCoset::new(domain.log_size()).step();
+            mask_entry
+                .iter()
+                .map(|mask_item| point + trace_step.mul_signed(*mask_item).into_ef())
+                .collect()
+        })
+        .collect()
 }
