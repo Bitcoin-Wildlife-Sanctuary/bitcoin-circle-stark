1b56f66e7481e639813f893ab76cca52a1be8847
weikengchen <w.k@berkeley.edu>
2024-10-08 06:43:02 +0400

    change WORM to LDM

--- src/dsl/plonk/part4_pair_vanishing_and_alphas.rs
+++ src/dsl/plonk/part4_pair_vanishing_and_alphas.rs
@@ -5,14 +5,14 @@ use bitcoin_script_dsl::builtins::qm31::QM31Var;
 use bitcoin_script_dsl::builtins::table::TableVar;
 use bitcoin_script_dsl::bvar::AllocVar;
 use bitcoin_script_dsl::constraint_system::{ConstraintSystem, ConstraintSystemRef};
-use bitcoin_script_dsl::worm::WORMMemory;
+use bitcoin_script_dsl::ldm::LDM;
 
-pub fn generate_cs(_: &Hints, worm: &mut WORMMemory) -> anyhow::Result<ConstraintSystemRef> {
+pub fn generate_cs(_: &Hints, ldm: &mut LDM) -> anyhow::Result<ConstraintSystemRef> {
     let cs = ConstraintSystem::new_ref();
-    worm.init(&cs)?;
+    ldm.init(&cs)?;
 
-    let oods_x: QM31Var = worm.read("oods_x")?;
-    let oods_y: QM31Var = worm.read("oods_y")?;
+    let oods_x: QM31Var = ldm.read("oods_x")?;
+    let oods_y: QM31Var = ldm.read("oods_y")?;
 
     let oods_point = SecureCirclePointVar {
         x: oods_x,
@@ -22,11 +22,11 @@ pub fn generate_cs(_: &Hints, worm: &mut WORMMemory) -> anyhow::Result<Constrain
     let table = TableVar::new_constant(&cs, ())?;
 
     let prepared_oods = prepare_pair_vanishing(&oods_point, &table);
-    worm.write("prepared_oods_a", &prepared_oods.0)?;
-    worm.write("prepared_oods_b", &prepared_oods.1)?;
+    ldm.write("prepared_oods_a", &prepared_oods.0)?;
+    ldm.write("prepared_oods_b", &prepared_oods.1)?;
 
-    let oods_shifted_by_1_x: QM31Var = worm.read("oods_shifted_by_1_x")?;
-    let oods_shifted_by_1_y: QM31Var = worm.read("oods_shifted_by_1_y")?;
+    let oods_shifted_by_1_x: QM31Var = ldm.read("oods_shifted_by_1_x")?;
+    let oods_shifted_by_1_y: QM31Var = ldm.read("oods_shifted_by_1_y")?;
 
     let oods_shifted_by_1_point = SecureCirclePointVar {
         x: oods_shifted_by_1_x,
@@ -34,16 +34,16 @@ pub fn generate_cs(_: &Hints, worm: &mut WORMMemory) -> anyhow::Result<Constrain
     };
 
     let prepared_oods_shifted_by_1 = prepare_pair_vanishing(&oods_shifted_by_1_point, &table);
-    worm.write(
+    ldm.write(
         "prepared_oods_shifted_by_1_a",
         &prepared_oods_shifted_by_1.0,
     )?;
-    worm.write(
+    ldm.write(
         "prepared_oods_shifted_by_1_b",
         &prepared_oods_shifted_by_1.1,
     )?;
 
-    let alpha: QM31Var = worm.read("line_batch_random_coeff")?;
+    let alpha: QM31Var = ldm.read("line_batch_random_coeff")?;
 
     // The needed alphas are:
     // - alpha, alpha^2, alpha^3, alpha^4, alpha^8, alpha^12, alpha^20
@@ -55,13 +55,13 @@ pub fn generate_cs(_: &Hints, worm: &mut WORMMemory) -> anyhow::Result<Constrain
     let alpha12 = &alpha8 * (&table, &alpha4);
     let alpha20 = &alpha8 * (&table, &alpha12);
 
-    worm.write("line_batch_random_coeff_2", &alpha2)?;
-    worm.write("line_batch_random_coeff_3", &alpha3)?;
-    worm.write("line_batch_random_coeff_4", &alpha4)?;
-    worm.write("line_batch_random_coeff_8", &alpha8)?;
-    worm.write("line_batch_random_coeff_12", &alpha12)?;
-    worm.write("line_batch_random_coeff_20", &alpha20)?;
+    ldm.write("line_batch_random_coeff_2", &alpha2)?;
+    ldm.write("line_batch_random_coeff_3", &alpha3)?;
+    ldm.write("line_batch_random_coeff_4", &alpha4)?;
+    ldm.write("line_batch_random_coeff_8", &alpha8)?;
+    ldm.write("line_batch_random_coeff_12", &alpha12)?;
+    ldm.write("line_batch_random_coeff_20", &alpha20)?;
 
-    worm.save()?;
+    ldm.save()?;
     Ok(cs)
 }
--- src/dsl/plonk/per_query_part2_num_trace.rs
+++ src/dsl/plonk/per_query_part2_num_trace.rs
@@ -7,43 +7,39 @@ use bitcoin_script_dsl::builtins::qm31::QM31Var;
 use bitcoin_script_dsl::builtins::table::TableVar;
 use bitcoin_script_dsl::bvar::AllocVar;
 use bitcoin_script_dsl::constraint_system::{ConstraintSystem, ConstraintSystemRef};
-use bitcoin_script_dsl::worm::WORMMemory;
+use bitcoin_script_dsl::ldm::LDM;
 
-pub fn generate_cs(
-    _: &Hints,
-    worm: &mut WORMMemory,
-    query_idx: usize,
-) -> Result<ConstraintSystemRef> {
+pub fn generate_cs(_: &Hints, ldm: &mut LDM, query_idx: usize) -> Result<ConstraintSystemRef> {
     let cs = ConstraintSystem::new_ref();
-    worm.init(&cs)?;
+    ldm.init(&cs)?;
 
-    let x: M31Var = worm.read(format!("circle_point_x_{}", query_idx))?;
-    let y: M31Var = worm.read(format!("circle_point_y_{}", query_idx))?;
+    let x: M31Var = ldm.read(format!("circle_point_x_{}", query_idx))?;
+    let y: M31Var = ldm.read(format!("circle_point_y_{}", query_idx))?;
     let table = TableVar::new_constant(&cs, ())?;
 
     let mut trace_queried_results = Vec::<(M31Var, M31Var)>::new();
     trace_queried_results.push((
-        worm.read(format!("trace_mult_queried_results_{}_l", query_idx))?,
-        worm.read(format!("trace_mult_queried_results_{}_r", query_idx))?,
+        ldm.read(format!("trace_mult_queried_results_{}_l", query_idx))?,
+        ldm.read(format!("trace_mult_queried_results_{}_r", query_idx))?,
     ));
     trace_queried_results.push((
-        worm.read(format!("trace_a_val_queried_results_{}_l", query_idx))?,
-        worm.read(format!("trace_a_val_queried_results_{}_r", query_idx))?,
+        ldm.read(format!("trace_a_val_queried_results_{}_l", query_idx))?,
+        ldm.read(format!("trace_a_val_queried_results_{}_r", query_idx))?,
     ));
     trace_queried_results.push((
-        worm.read(format!("trace_b_val_queried_results_{}_l", query_idx))?,
-        worm.read(format!("trace_b_val_queried_results_{}_r", query_idx))?,
+        ldm.read(format!("trace_b_val_queried_results_{}_l", query_idx))?,
+        ldm.read(format!("trace_b_val_queried_results_{}_r", query_idx))?,
     ));
     trace_queried_results.push((
-        worm.read(format!("trace_c_val_queried_results_{}_l", query_idx))?,
-        worm.read(format!("trace_c_val_queried_results_{}_r", query_idx))?,
+        ldm.read(format!("trace_c_val_queried_results_{}_l", query_idx))?,
+        ldm.read(format!("trace_c_val_queried_results_{}_r", query_idx))?,
     ));
 
     let mut column_line_trace_vars = Vec::<(CM31Var, CM31Var)>::new();
     for i in 0..4 {
         column_line_trace_vars.push((
-            worm.read(format!("column_line_coeffs_trace_{}_a", i))?,
-            worm.read(format!("column_line_coeffs_trace_{}_b", i))?,
+            ldm.read(format!("column_line_coeffs_trace_{}_a", i))?,
+            ldm.read(format!("column_line_coeffs_trace_{}_b", i))?,
         ))
     }
 
@@ -83,9 +79,9 @@ pub fn generate_cs(
         &column_line_trace_vars[3].1,
     );
 
-    let alpha3: QM31Var = worm.read("line_batch_random_coeff_3")?;
-    let alpha2: QM31Var = worm.read("line_batch_random_coeff_2")?;
-    let alpha: QM31Var = worm.read("line_batch_random_coeff")?;
+    let alpha3: QM31Var = ldm.read("line_batch_random_coeff_3")?;
+    let alpha2: QM31Var = ldm.read("line_batch_random_coeff_2")?;
+    let alpha: QM31Var = ldm.read("line_batch_random_coeff")?;
 
     let mut sum_num_trace_l = &alpha3 * (&table, &numerator_trace_mult.0);
     sum_num_trace_l = &sum_num_trace_l + &(&alpha2 * (&table, &numerator_trace_a_val.0));
@@ -97,28 +93,28 @@ pub fn generate_cs(
     sum_num_trace_r = &sum_num_trace_r + &(&alpha * (&table, &numerator_trace_b_val.1));
     sum_num_trace_r = &sum_num_trace_r + &numerator_trace_c_val.1;
 
-    let alpha20: QM31Var = worm.read("line_batch_random_coeff_20")?;
+    let alpha20: QM31Var = ldm.read("line_batch_random_coeff_20")?;
 
     let alpha20trace_l = &alpha20 * (&table, &sum_num_trace_l);
     let alpha20trace_r = &alpha20 * (&table, &sum_num_trace_r);
 
-    worm.write(format!("alpha20trace_{}_l", query_idx), &alpha20trace_l)?;
-    worm.write(format!("alpha20trace_{}_r", query_idx), &alpha20trace_r)?;
+    ldm.write(format!("alpha20trace_{}_l", query_idx), &alpha20trace_l)?;
+    ldm.write(format!("alpha20trace_{}_r", query_idx), &alpha20trace_r)?;
 
-    let prepared_oods_a: CM31Var = worm.read("prepared_oods_a")?;
-    let prepared_oods_b: CM31Var = worm.read("prepared_oods_b")?;
+    let prepared_oods_a: CM31Var = ldm.read("prepared_oods_a")?;
+    let prepared_oods_b: CM31Var = ldm.read("prepared_oods_b")?;
 
     let denominator_inverse_var =
         denominator_inverse_from_prepared(&table, &prepared_oods_a, &prepared_oods_b, &x, &y);
-    worm.write(
+    ldm.write(
         format!("denominator_inverse_{}_l", query_idx),
         &denominator_inverse_var.0,
     )?;
-    worm.write(
+    ldm.write(
         format!("denominator_inverse_{}_r", query_idx),
         &denominator_inverse_var.1,
     )?;
 
-    worm.save()?;
+    ldm.save()?;
     Ok(cs)
 }
